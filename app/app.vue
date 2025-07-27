bf213fd6aaa93d98b98aa3ab12d5fac43fd098a4
FelyCZ <jakub.feren@gmail.com>
2025-07-27 21:05:46 +0200

    refactor: remove unused code

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,5 @@
 <script setup lang="ts">
-// Set custom anchor with Y axis scrolling to dynamic offset
-// nuxtApp.$anchorScroll!.defaults.toAnchor = () => ({
-// 	behavior: "smooth",
-// 	offsetTop: -(toValue(useNuxtApp().$headerHeight) ?? 0) * 1.2,
-// });
 
-// // Add route specialization for fixed solution
-// // See docs for explanation
-// nuxtApp.$anchorScroll!.matched.push(({ path, hash }) => {
-// 	if (hash) {
-// 		// All anchor element on this route is mangled
-// 		const targetSelector = `#fixed-${hash.slice(1)}`;
-// 		const targetElement = document.querySelector(targetSelector);
-// 		if (targetElement) {
-// 			return {
-// 				toAnchor: {
-// 					target: targetElement as HTMLElement,
-// 					scrollOptions: toValue(useNuxtApp().$anchorScroll?.defaults?.toAnchor) ?? {},
-// 				},
-// 			};
-// 		}
-// 	}
-// });
 </script>
 
 <template>
