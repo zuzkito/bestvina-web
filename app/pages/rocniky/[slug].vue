fffe6bdbe2fcdd41d2d32928a8991ad373f3327e
FelyCZ <jakub.feren@gmail.com>
2025-07-28 21:28:28 +0200

    feat: add group images to archive pages

--- app/pages/rocniky/[slug].vue
+++ app/pages/rocniky/[slug].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { CURRENT_YEAR } from "~/app.config";
+import ImageModal from "~/components/DownloadImageModal.vue";
+import Placeholder from "~/components/Placeholder.vue";
 
 definePageMeta({
 	layout: "page",
@@ -9,6 +11,19 @@ const route = useRoute();
 const { data: page } = await useAsyncData(route.path, () => {
 	return queryCollection("years").path(route.path).first();
 });
+
+const isCurrentYear = ref(page.value?.year == CURRENT_YEAR || false);
+
+const { data: groupPhotoPaths, status, error } = await useFetch(`/api/images/group/${page.value?.year}`);
+
+const overlay = useOverlay();
+const modal = overlay.create(ImageModal);
+
+async function openImageModal(path: string) {
+	modal.open({
+		imgPath: path,
+	});
+}
 
 const surround = await getSurroundings();
 async function getSurroundings() {
@@ -37,12 +52,39 @@ async function getSurroundings() {
 		</template> -->
 
 		<UPageHeader
-			:title="`${page?.year == CURRENT_YEAR.toString() ? 'Aktuální' : page?.theme}`"
+			:title="`${isCurrentYear ? 'Aktuální ročník' : page?.theme}`"
+			:description="`${isCurrentYear ? 'Táborové téma se dozvíš až na začátku soustředění!' : ''}`"
 			:headline="`Ročník ${page?.year}`"
 		/>
 
 		<UPageBody>
-			<h1>{{ page?.seo }}</h1>
+			<Placeholder class="w-full h-50" />
+
+			<div v-show="!isCurrentYear">
+				<USeparator
+					icon="i-mdi-history"
+				/>
+
+				<UPageHeader
+					title="Oddíly"
+					:ui="{
+						title: 'text-2xl sm:text-3xl',
+					}"
+				/>
+				<div class="flex flex-wrap gap-8 py-4 justify-evenly">
+					<a
+						v-for="photoPath in groupPhotoPaths"
+						:key="photoPath"
+						@click="openImageModal(photoPath)"
+					>
+						<NuxtImg
+							:src="photoPath"
+							height="300"
+							class="rounded-md md:hover:scale-110 transition-transform"
+						/>
+					</a>
+				</div>
+			</div>
 
 			<USeparator v-if="surround?.filter(Boolean).length" />
 			<UContentSurround :surround="surround" />
