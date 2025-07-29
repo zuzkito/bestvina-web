22b3836cefc5cc39ac081a210feebeb62432dab4
FelyCZ <jakub.feren@gmail.com>
2025-07-29 19:41:12 +0200

    fix: reconfigure eslint for better support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,22 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  // Your custom configs here
-)
+	// Your custom configs here
+	{
+		rules: {
+			"style/*": "off",
+			"@stylistic/*": "off",
+			"format/*": "off",
+			"*-indent": "off",
+			"*-spacing": "off",
+			"*-spaces": "off",
+			"*-order": "off",
+			"*-dangle": "off",
+			"*-newline": "off",
+			"*quotes": "off",
+			"*semi": "off",
+			"object-curly-spacing": ["error", "always"],
+		},
+	},
+);
