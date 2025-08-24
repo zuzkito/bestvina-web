7fb13b40678ab1d533873733289ea4582e49dd1b
FelyCZ <jakub.feren@gmail.com>
2025-08-25 00:39:09 +0200

    feat: custom bestvina-themed spinner loader idea

---
+++ app/components/BestLoader.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+// TODO: Animated SVG -- titration color change in flask.
+//  Flask with a liquid inside.
+//  Falling droplets (or dripping from eyedropper) that makes the liquid in flask change to purple.
+//  A little shake effect that makes the liquid in flask change back to colorless.
+//  Repeat the process.
+//
+//
+// Possible software: https://creator.lottiefiles.com/
+</script>
+
+<template>
+	<div />
+</template>
+
+<style scoped></style>
