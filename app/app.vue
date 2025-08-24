6a991919ad7b6f10d41a2d233eaad719e2a43535
FelyCZ <jakub.feren@gmail.com>
2025-08-25 00:33:45 +0200

    feat: infinite scroll container

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,28 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { onMounted } from "#imports";
 
+const appContainer = useTemplateRef<HTMLElement>("app-container");
+onMounted(() => {
+	useNuxtApp().provide("appContainer", appContainer);
+	console.log(appContainer.value);
+});
 </script>
 
 <template>
-	<UApp>
-		<NuxtLoadingIndicator
-			color="var(--ui-secondary)"
-			:height="2"
-		/>
+	<div
+		ref="app-container"
+		class="h-[100vh] overflow-y-auto"
+	>
+		<UApp>
+			<NuxtLoadingIndicator
+				:height="
+					2"
+				color="var(--ui-secondary)"
+			/>
 
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</UApp>
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
+		</UApp>
+	</div>
 </template>
