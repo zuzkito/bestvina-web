e8072003173845c797c61d36db9ade6d3880c83d
FelyCZ <jakub.feren@gmail.com>
2025-12-02 01:03:45 +0100

    feat & refactor: add dynamic content renderer, adjust subheaders

--- app/pages/rocniky/[slug].vue
+++ app/pages/rocniky/[slug].vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { CURRENT_YEAR } from "~/app.config";
-import ImageModal from "~/components/DownloadImageModal.vue";
-import Placeholder from "~/components/Placeholder.vue";
+import { CURRENT_YEAR } from "#shared/constants";
+import Placeholder from "~/components/PlaceHolder.vue";
+import { CalendarDate } from "@internationalized/date";
 
 definePageMeta({
 	layout: "page",
@@ -12,26 +12,21 @@ const { data: page } = await useAsyncData(route.path, () => {
 	return queryCollection("years").path(route.path).first();
 });
 
-const isCurrentYear = ref(page.value?.year == CURRENT_YEAR || false);
+const startDate = new Date(page.value?.startDate ?? "");
+const endDate = new Date(page.value?.endDate ?? "");
+const calendarDate = shallowRef({
+	start: new CalendarDate(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()),
+	end: new CalendarDate(endDate.getFullYear(), endDate.getMonth(), endDate.getDate()),
+});
 
-const { data: groupPhotoPaths } = await useFetch(`/api/images/group/${page.value?.year}`);
-const { data: galleryPreviewPhotos } = await useFetch(`/api/images/gallery/${page.value?.year}/random?n=10`);
-
-const isGalleryAvailable = computed(() => galleryPreviewPhotos?.value?.length != 0);
-
-// prepare image modal for viewing and downloading
-const overlay = useOverlay();
-const modal = overlay.create(ImageModal);
-
-async function openImageModal(path: string) {
-	modal.open({
-		imgPath: path,
-	});
+if (!page.value || !page.value.year) {
+	throw createError({ statusCode: 404, statusMessage: "Ročník nenalezen!", fatal: true });
 }
+
+const isCurrentYear = ref(page.value?.year == CURRENT_YEAR || false);
 
 // get surroundings for navigation
 const surround = await getSurroundings();
-
 async function getSurroundings() {
 	const { data: surround } = await useAsyncData(`${route.path}-surround`, () => {
 		return queryCollectionItemSurroundings("years", route.path, {
@@ -49,124 +44,71 @@ async function getSurroundings() {
 	});
 	return surround;
 }
+
+const scrollToTop = () => {
+	const appContainer = useNuxtApp().$appContainer as Ref<HTMLElement> || undefined;
+	if (!appContainer)	return;
+
+	appContainer.value.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+// scroll to top of the page after going back/forward using surroundings
+watch(page, (newPage, oldPage) => {
+	if (newPage !== oldPage)
+		scrollToTop();
+}, { immediate: true });
 </script>
 
 <template>
-	<UPage>
-		<!-- <template #left>
-      <UPageAside />
-    </template> -->
-
+	<UPage
+		v-if="page"
+		id="pageId"
+	>
 		<UPageHeader
 			:description="`${isCurrentYear ? 'Táborové téma se dozvíš až na začátku soustředění!' : ''}`"
-			:headline="`Ročník ${page?.year}`"
-			:title="`${isCurrentYear ? 'Aktuální ročník' : page?.theme}`"
+			:headline="`Ročník ${page.year}`"
+			:title="`${isCurrentYear ? 'Aktuální ročník' : page.theme}`"
 		/>
-
 		<UPageBody>
 			<Placeholder class="w-full h-50" />
+
+			<!--			<UCalendar -->
+			<!--				v-model="calendarDate" -->
+			<!--				:number-of-months="2" -->
+			<!--				:year-controls="false" -->
+			<!--				class="w-1/3" -->
+			<!--				range -->
+			<!--			/> -->
+
+			<ContentRenderer :value="page.body" />
 
 			<div v-show="!isCurrentYear">
 				<USeparator icon="i-mdi-history" />
 
 				<!-- Gallery -->
-				<div v-show="isGalleryAvailable">
-					<UPageHeader
-						:ui="{
-							title: 'text-2xl sm:text-3xl',
-						}"
-						description="V karuselu se zobrazuje 10 náhodných fotografií z daného roku."
-						title="Náhled galerie"
-					/>
-					<NuxtLink :to="`${route.path}/galerie`">
-						<UAlert
-							class="hover:border-dashed hover:border"
-							icon="i-lucide-gallery-thumbnails"
-							title="Chceš-li zobrazit celou galerii, klikni zde!"
-							variant="subtle"
-						/>
-					</NuxtLink>
-					<UCarousel
-						v-slot="{ item }"
-						:autoplay="{
-							delay: 5000,
-							stopOnInteraction: true,
-						}"
-						:items="galleryPreviewPhotos"
-						:ui="{
-							container: 'gap-0 p-0 ms-0',
-							item: 'basis-full lg:basis-1/3 w-fit p-0 flex flex-row gap-0 justify-center',
-						}"
-						class="w-full mt-8 mb-16"
-						dots
-						loop
-					>
-						<div class="m-4 w-full aspect-square lg:aspect-auto">
-							<NuxtImg
-								v-slot="{ src, isLoaded, imgAttrs }"
-								:custom="true"
-								:src="item"
-							>
-								<!-- Show the actual image when loaded -->
-								<div
-									v-if="isLoaded"
-									class="p-4"
-								>
-									<img
-										:key="item"
-										:alt="`náhodná fotografie z roku ${page?.year}`"
-										:src="src"
-										class="w-full aspect-square object-cover rounded-md md:hover:scale-110 transition-transform"
-										loading="lazy"
-										v-bind="imgAttrs"
-										@click="openImageModal(item)"
-									>
-								</div>
 
-								<!-- Show a placeholder while loading -->
-								<USkeleton
-									v-else
-									class="w-full aspect-square"
-								/>
-							</NuxtImg>
-						</div>
-					</UCarousel>
-
-					<USeparator
-						class="my-4"
-					/>
-				</div>
+				<LazyGradeGalleryPreview
+					:year="page.year.toString()"
+				/>
 
 				<!-- Group Photos -->
-				<div>
-					<UPageHeader
-						:ui="{
-							title: 'text-2xl sm:text-3xl',
-						}"
-						title="Fotografie oddílů"
-					/>
-					<div class="flex flex-row flex-wrap gap-8 py-4 justify-evenly">
-						<a
-							v-for="photoPath in groupPhotoPaths"
-							:key="photoPath"
-							@click="openImageModal(photoPath)"
-						>
-							<NuxtImg
-								:src="photoPath"
-								class="rounded-md md:hover:scale-110 transition-transform"
-								height="300"
-								lazy="blur"
-								loading="lazy"
-							/>
-						</a>
-					</div>
-				</div>
-				<USeparator
-					v-if="surround?.filter(Boolean).length"
-					class="mt-4 mb-8"
+				<LazyGradeGroupImages
+					:year="page.year.toString()"
 				/>
-				<UContentSurround :surround="surround" />
+
+				<!-- Surroundings -->
+				<!--				<USeparator -->
+				<!--					v-if="surround?.filter(Boolean).length" -->
+				<!--					class="mt-4 mb-8" -->
+				<!--				/> -->
+				<UContentSurround
+					:surround="surround"
+					class="mt-16"
+				/>
 			</div>
 		</UPageBody>
 	</UPage>
 </template>
+
+<style scoped>
+</style>
