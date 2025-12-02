506ec380d4db4e58758e31a1f58e2591e4f17abf
FelyCZ <jakub.feren@gmail.com>
2025-12-02 01:12:58 +0100

    feat: add banner, change locale

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,23 @@
 <script lang="ts" setup>
 import { onMounted } from "#imports";
+import { cs } from "@nuxt/ui/locale";
 
 const appContainer = useTemplateRef<HTMLElement>("app-container");
 onMounted(() => {
 	useNuxtApp().provide("appContainer", appContainer);
-	console.log(appContainer.value);
+});
+
+useHead({
+	meta: [
+		{ name: "viewport", content: "width=device-width, initial-scale=1" },
+	],
+	link: [
+		{ rel: "icon", href: "/favicon.ico" },
+	],
+	htmlAttrs: {
+		lang: "cs",
+	},
+	title: "Běstvina",
 });
 </script>
 
@@ -13,13 +26,20 @@ onMounted(() => {
 		ref="app-container"
 		class="h-[100vh] overflow-y-auto"
 	>
-		<UApp>
+		<UApp :locale="cs">
 			<NuxtLoadingIndicator
-				:height="
-					2"
-				color="var(--ui-secondary)"
+				:height="2"
+				color="var(--ui-primary)"
 			/>
-
+			<UBanner
+				:ui="{
+					root: 'py-2',
+					title: 'line-clamp-3 whitespace-normal',
+				}"
+				class="h-fit"
+				close
+				title="Web je stále v raných fázích vývoje a nemusí zatím fungovat zcela podle představ..."
+			/>
 			<NuxtLayout>
 				<NuxtPage />
 			</NuxtLayout>
