18a6d0268406f67b58e8c20e30414ccb9feffe93
FelyCZ <jakub.feren@gmail.com>
2025-12-02 01:13:07 +0100

    feat: promo images

---
+++ app/components/CampMap.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+	<div
+		class="flex justify-center"
+	>
+		<iframe
+			class="w-full border-0 ring-2 ring-primary-100 rounded-xl"
+			height="466"
+			src="https://mapy.com/s/jefecupara"
+		/>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
