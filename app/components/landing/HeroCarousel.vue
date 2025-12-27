c9ac7709be783a7941b8723332acc433b5e3af34
FelyCZ <jakub.feren@gmail.com>
2025-12-27 22:08:36 +0100

    refactor: update hero carousel

--- app/components/landing/HeroCarousel.vue
+++ app/components/landing/HeroCarousel.vue
@@ -8,14 +8,14 @@ const placeholder = img(src, {}, { preset: "thumbnailXXSm" });
 	<!--  TODO: make this a carousel -->
 	<div
 		class="h-full w-full lg:hover:scale-105 transition-transform rounded-xl
-		light:bg-gradient-to-br light:from-primary-400 light:via-tertiary-500/70 light:to-secondary-500/80
-		dark:bg-gradient-to-br dark:from-primary-400/70 dark:via-tertiary-500/40 dark:to-secondary-500/60"
+		light:bg-linear-to-br light:from-primary-400 light:via-tertiary-500/70 light:to-secondary-500/80
+		dark:bg-linear-to-br dark:from-primary-400/70 dark:via-tertiary-500/40 dark:to-secondary-500/60"
 	>
 		<NuxtImg
 			:placeholder="placeholder"
 			:src="src"
-			class="h-full w-full object-cover object-[75%_25%] rounded-xl p-1"
-			loading="lazy"
+			class="h-96 lg:h-full w-full object-cover object-[75%_25%] rounded-xl p-1"
+			fetch-priority="high"
 			preset="thumbnailXXLg"
 		/>
 	</div>
