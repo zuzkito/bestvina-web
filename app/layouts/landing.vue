08dce0c13953a23a200c1dd097a32200811a2f72
FelyCZ <jakub.feren@gmail.com>
2026-02-20 01:31:09 +0100

    fix: layout structure improvement

--- app/layouts/landing.vue
+++ app/layouts/landing.vue
@@ -2,9 +2,9 @@
 	<div>
 		<AppHeader />
 		<!-- make the page fill windows height even if there is not enough content -->
-		<div class="min-h-(--my-page-height)">
+		<UMain class="min-h-(--my-page-height)">
 			<slot />
-		</div>
+		</UMain>
 
 		<AppFooter />
 	</div>
