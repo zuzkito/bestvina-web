e87a5271ad1406dd1139565b4edee7d5ed66d5d0
FelyCZ <jakub.feren@gmail.com>
2026-02-20 01:35:46 +0100

    fix: timeline component after updating NuxtUI module

--- app/components/about/TimelineContainer.vue
+++ app/components/about/TimelineContainer.vue
@@ -9,23 +9,17 @@ defineProps<{
 		:items="items"
 		:ui="{
 			root: 'mt-4 md:mt-8',
-			date: 'float-end ms-1',
-			description: ' rounded-md text-default',
+			date: 'text-secondary',
+			title: 'text-highlighted',
+			description: 'text-muted',
 			indicator: 'bg-secondary',
 			separator: 'bg-secondary/25',
 		}"
 		class="w-full"
 		size="3xs"
 	>
-		<template #title="{ item }">
-			<div class="flex flex-col">
-				<span class="text-secondary">{{ item.time }}</span>
-				<span class="text-highlighted">{{ item.title }}</span>
-			</div>
-		</template>
-
 		<template #date="{ item }">
-			{{ item.description }}
+			{{ item.time }}
 		</template>
 	</UTimeline>
 </template>
